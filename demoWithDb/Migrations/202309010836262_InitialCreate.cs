b0ac46b71b0e12afc339eadb51bd9bfeae073d6f
Jp8125 <190170116055@vgec1.gujgov.edu.in>
2023-09-25 12:11:06 +0530

    Add project files.

---
+++ demoWithDb/Migrations/202309010836262_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿namespace demoWithDb.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Students",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Department = c.String(),
+                        EmailId = c.String(),
+                        Genter = c.String(),
+                        DOB = c.DateTime(),
+                        Address = c.String(),
+                        City = c.String(),
+                        State = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Students");
+        }
+    }
+}
---
+++ demoWithDb/Migrations/202309010859058_second.cs
@@ -0,0 +1,20 @@
+﻿namespace demoWithDb.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class second : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Students", "Gender", c => c.String());
+            DropColumn("dbo.Students", "Genter");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Students", "Genter", c => c.String());
+            DropColumn("dbo.Students", "Gender");
+        }
+    }
+}
